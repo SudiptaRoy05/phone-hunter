e6ec0032e11c1dab18beebcff9f6e615d9fe2e6d
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-04 12:24:54 +0600

    triying to understand

--- hunter.js
+++ hunter.js
@@ -1,12 +1,36 @@
-const loadAllPhones = ()=>{
-    console.log('3 sec gone');
+
+const loadAllPhones = async (status, bName) => {
+    document.getElementById("loader").classList.add('d-none');
+
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${bName ? bName : 'iphone'}`);
+        const data = await res.json();
+        status ? displayAllPhones(data.data) : displayAllPhones(data.data.slice(0,6));
+        
+    }
+    catch(error){
+        console.log(error)
+    }
+
+    console.log(status)
+}
+
+const displayAllPhones = (phones)=>{
+    console.log(phones)
+}
+
+
+const handleShowAll = ()=>{
+    loadAllPhones(true);
 }
 
 
 
 const handleSearch = () => {
-    console.log('first')
-    setTimeout(()=>{
-        loadAllPhones();
-    },3000)
+    document.getElementById("loader").classList.remove('d-none');
+    const searchTxt =  document.getElementById('search-field').value;
+    setTimeout(() => {
+        loadAllPhones(false, searchTxt);
+
+    }, 2500)
 }
